09f3249e815c2ded05dd14b37b21925bd1bc2bb5
mleitem <megan.leitem@yahoo.com>
2022-04-25 13:56:13 -0700

    Added Dashboard.fxml and DashboardController

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,9 +1,13 @@
 package Controller;
 
+import Helper.JDBC;
 import Model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -11,10 +15,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
+    Stage stage;
+    Parent scene;
 
     @FXML
     private PasswordField passwordid;
@@ -32,6 +41,7 @@ public class LoginController implements Initializable {
     @FXML
     public void exitButton(ActionEvent event) throws IOException {
         Stage closeStage = (Stage) exitbuttonid.getScene().getWindow();
+        JDBC.closeConnection();
         closeStage.close();
     }
 
@@ -40,9 +50,33 @@ public class LoginController implements Initializable {
         String username = usernameid.getText();
         String password = passwordid.getText();
 
-        User.login(username, password);
+        if (login(username, password) == true) {
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/"));
+            scene = loader.load();
+            Scene root = new Scene(scene);
+            stage.setScene(root);
+            stage.show();
+        }
     }
 
+    public boolean login(String checkUsername, String checkPassword) throws SQLException {
+        String sql = "SELECT * FROM users WHERE User_Name = ? AND Password = ?";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ps.setString(1, checkUsername);
+        ps.setString(2, checkPassword);
+        ResultSet rs = ps.executeQuery();
+        boolean login;
+        if (rs.next()) {
+            System.out.println("Success!");
+            login = true;
+        }
+        else{
+            System.out.println("Try again!");
+            login = false;
+        }
+        return login;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/Model/User.java
+++ src/Model/User.java
@@ -13,17 +13,19 @@ public class User {
     private String username;
     private String password;
 
-    public static void login(String checkUsername, String checkPassword) throws SQLException {
+    /*public static void login(String checkUsername, String checkPassword) throws SQLException {
         String sql = "SELECT * FROM users WHERE User_Name = ? AND Password = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
         ps.setString(1, checkUsername);
         ps.setString(2, checkPassword);
         ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            String printUsername = rs.getString("User_Name");
-            String printPasswod = rs.getString("Password");
-            System.out.print(printUsername + " | ");
-            System.out.print(printPasswod + "\n");
+        if (rs.next()) {
+            System.out.println("Success!");
+
+
         }
-    }
+        else{
+            System.out.println("Try again!");
+        }
+    }*/
 }

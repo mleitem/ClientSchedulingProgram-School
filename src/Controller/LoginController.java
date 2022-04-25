9709d46c7891dd4e54217963f1ed66f6e2f2e483
mleitem <megan.leitem@yahoo.com>
2022-04-25 13:33:20 -0700

    Added functionality to buttons on Login page

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,4 +1,51 @@
 package Controller;
 
-public class LoginController {
+import Model.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private PasswordField passwordid;
+
+    @FXML
+    private TextField usernameid;
+
+    @FXML
+    private Button exitbuttonid;
+
+    @FXML
+    private Button loginbuttonid;
+
+    /** When this event handler is called, the program will exit. */
+    @FXML
+    public void exitButton(ActionEvent event) throws IOException {
+        Stage closeStage = (Stage) exitbuttonid.getScene().getWindow();
+        closeStage.close();
+    }
+
+    @FXML
+    public void loginButton(ActionEvent event) throws IOException, SQLException {
+        String username = usernameid.getText();
+        String password = passwordid.getText();
+
+        User.login(username, password);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,34 @@ package com.company;
 import Helper.CustomerQuery;
 import Helper.JDBC;
 import Model.User;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) { launch(args); }
 
+    /*public static void main(String[] args) throws SQLException {
         JDBC.openConnection();
 
-        User.login("admin", "admin");
+
 
         JDBC.closeConnection();
+    } */
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        JDBC.openConnection();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Login.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }

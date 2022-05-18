7cb6965562773c2742f27d138fba7ec2c1088bb2
mleitem <megan.leitem@yahoo.com>
2022-05-18 14:18:19 -0700

    Added listener for country combo box to UpdateCustomer

--- src/Controller/UpdateCustomerController.java
+++ src/Controller/UpdateCustomerController.java
@@ -77,13 +77,75 @@ public class UpdateCustomerController implements Initializable {
         //PICK UP HERE! Isolated country ID, need to do the same for div ID
         ObservableList<Integer> countries = (CustomerQuery.getCountry(customer.getDivId()));
         int countryId = countries.get(0);
+        if(countryId == 1){
+            ObservableList<String> usRegions = CustomerQuery.viewUSRegions();
+            stateid.setItems(usRegions);
+        }
+        if(countryId == 2){
+            ObservableList<String> ukRegions = CustomerQuery.viewUKRegions();
+            stateid.setItems(ukRegions);
+        }
+        if(countryId == 3){
+            ObservableList<String> canadaRegions = CustomerQuery.viewCanadaRegions();
+            stateid.setItems(canadaRegions);
+        }
         String id = String.valueOf(countryId);
         countryid.setValue(id);
 
+
     }
+
+    public void regionFilter(ActionEvent event) throws SQLException {
+
+            countryid.setOnAction(event1 -> {
+                String id = countryid.getValue();
+                if(id.contains("1")){
+                    ObservableList<String> usRegions = null;
+                    try {
+                        usRegions = CustomerQuery.viewUSRegions();
+                        stateid.setItems(usRegions);
+                        stateid.setValue("1: Alabama");
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                }
+                if(id.contains("2")) {
+                    ObservableList<String> ukRegions = null;
+                    try {
+                        ukRegions = CustomerQuery.viewUKRegions();
+                        stateid.setItems(ukRegions);
+                        stateid.setValue("101: England");
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+
+                }
+                if(id.contains("3")){
+                    ObservableList<String> canadaRegions = null;
+                    try {
+                        canadaRegions = CustomerQuery.viewCanadaRegions();
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                    stateid.setItems(canadaRegions);
+                    stateid.setValue("60: Northwest Territories");
+                }
+
+            });
+
+
+            }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        ObservableList<String> allCountries = null;
+        try {
+            allCountries = CustomerQuery.viewAllCountries();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        countryid.setItems(allCountries);
     }
 }
